6e23fdfbabb45f0642676863a378f372dc86457d
dabanguai <1596815312@qq.com>
2024-05-06 18:33:28 +0800

    利用MybatisX生成question表和question_submit表的代码

---
+++ src/main/java/generator/mapper/QuestionSubmitMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.QuestionSubmit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 15968
+* @description 针对表【question_submit(题目提交)】的数据库操作Mapper
+* @createDate 2024-05-06 18:31:43
+* @Entity generator.domain.QuestionSubmit
+*/
+public interface QuestionSubmitMapper extends BaseMapper<QuestionSubmit> {
+
+}
+
+
+
+
